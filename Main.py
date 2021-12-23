9b94aad77a55cb03a7b2f15d6c872fac1a58e9e2
Isaiah Magana <Maganaisaiah4@gmail.com>
2021-12-23 12:53:51 -0500

    Created, and finished base project

---
+++ Main.py
@@ -0,0 +1,38 @@
+#Unit 1 Challange 5 Multipacation / Exponent Table
+
+print("Welcome to the Multipacation / Exponent Table App")
+print()
+
+#Get user info and cast to number
+username = input("What is your name: ")
+number = float(input("What number would you like to work with: "))
+
+#Multipacation table output
+print(f"\nMultipacation table for {number}")
+print(f"\t1.0 * {number} = {1.0 * number}")
+print(f"\t2.0 * {number} = {2.0 * number}")
+print(f"\t3.0 * {number} = {3.0 * number}")
+print(f"\t4.0 * {number} = {4.0 * number}")
+print(f"\t5.0 * {number} = {5.0 * number}")
+print(f"\t6.0 * {number} = {6.0 * number}")
+print(f"\t7.0 * {number} = {7.0 * number}")
+print(f"\t8.0 * {number} = {8.0 * number}")
+print(f"\t9.0 * {number} = {9.0 * number}")
+
+#Exponent table Ouput
+print(f"\nExponent Table for {number}")
+print(f"\t{number}**1.0 = {number**1.0}")
+print(f"\t{number}**2.0 = {number**2.0}")
+print(f"\t{number}**3.0 = {number**3.0}")
+print(f"\t{number}**4.0 = {number**4.0}")
+print(f"\t{number}**5.0 = {number**5.0}")
+print(f"\t{number}**6.0 = {number**6.0}")
+print(f"\t{number}**7.0 = {number**7.0}")
+print(f"\t{number}**8.0 = {number**8.0}")
+print(f"\t{number}**9.0 = {number**9.0}")
+
+#Ending Output
+print(f"{username} Math is cool!")
+print(f"\t{username} math is cool!")
+print(f"\t\t{username} Math Is Cool!")
+print(f"\t\t\t{username.upper()} MATH IS COOL!")
